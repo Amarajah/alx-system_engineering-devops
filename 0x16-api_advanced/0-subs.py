6ede2646b02977624bbface72078a54b58434773
Amarajah <naomi.ibeh69@gmail.com>
2023-05-11 15:14:10 -0700

    Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Write a function that queries the Reddit API 
+and returns the number of subscribers 
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+
+Hint: No authentication is necessary for most features of the Reddit API.
+If you’re getting errors related to Too Many Requests,
+ensure you’re setting a custom User-Agent.
+'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''Returns number of subscribers'''
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
+        return 0
